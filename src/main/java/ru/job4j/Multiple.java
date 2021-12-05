b1ad5b29695f6f004b9c168d391f42d4d0f50bab
Andrey Morozov <selove.jara@gmail.com>
2021-12-05 21:35:40 +0400

    4.2. Заголовок в файле java [#159036 #205319]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("9*1=9");
+        System.out.println("9*2=18");
+        System.out.println("9*3=27");
+        System.out.println("9*4=36");
+        System.out.println("9*5=45");
+        System.out.println("9*6=54");
+        System.out.println("9*7=63");
+        System.out.println("9*8=72");
+        System.out.println("9*9=81");
+        System.out.println("9*10=90");
+    }
+}
+
--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,5 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Andrey Morozov");
+        System.out.println("24.01.1998");
     }
 }
