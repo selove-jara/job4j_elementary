f68d1583e9a529ae0723a9da0443345d819725e7
Andrey Morozov <selove.jara@gmail.com>
2021-12-18 21:28:53 +0400

    4.3.3. Логическое отрицание ! [#334072 #241427]

--- src/main/java/ru/job4j/condition/LogicNot.java
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -15,15 +15,15 @@ public class LogicNot {
     }
 
     public static boolean notPositive(int num) {
-        return num < 0;
+        return !isPositive(num);
     }
 
     public static boolean notEvenAndPositive(int num) {
-        return  !isEven(num) && num > 0;
+        return  notEven(num) && isPositive(num);
     }
 
     public static boolean evenOrNotPositive(int num) {
-        return num % 2 == 0 || num < 0;
+        return isEven(num) || notPositive(num);
     }
 
 }
