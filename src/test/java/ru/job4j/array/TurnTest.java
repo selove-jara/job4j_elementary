ced46342552066e44357f9c4c90e9aa30d31255c
Andrey Morozov <selove.jara@gmail.com>
2022-03-13 18:17:47 +0400

    6.2.3. Перевернуть массив. [#226790]

---
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TurnTest {
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[]{4, 1, 6, 2};
+        int[] result = Turn.back(input);
+        int[] expected = new int[]{2, 6, 1, 4};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray1() {
+        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] result = Turn.back(input);
+        int[] expected = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        int[] input = new int[]{1, 2, 3, 4, 5};
+        int[] result = Turn.back(input);
+        int[] expected = new int[]{5, 4, 3, 2, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray1() {
+        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int[] result = Turn.back(input);
+        int[] expected = new int[]{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+}
