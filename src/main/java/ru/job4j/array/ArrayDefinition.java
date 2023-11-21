21c782bd5f65147492609faa893fe1e403be454f
Andrey Morozov <selove.jara@gmail.com>
2023-11-21 11:59:34 +0400

    6.0.2. Размер массива [#333582 #273951]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         float[] prices = new float[40];
         String[] surnames = new String[100500];
+        System.out.println(ages.length);
+        System.out.println(prices.length);
+        System.out.println(surnames.length);
     }
 }
