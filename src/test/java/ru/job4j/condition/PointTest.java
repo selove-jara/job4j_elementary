0cdcba95e7e4f9f63f7b5cf804e50e124fd44d9e
Andrey Morozov <selove.jara@gmail.com>
2023-12-04 20:28:15 +0400

    2.0. Рефакторинг - Расстояние между точками. [#122587 #285749]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -11,44 +11,36 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when20to20then1() {
         double expected = 1;
-        int x1 = 2;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(2, 0);
+        Point b = new Point(2, 1);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when03to460then5() {
         double expected = 5;
-        int x1 = 0;
-        int y1 = 3;
-        int x2 = 4;
-        int y2 = 6;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(0, 3);
+        Point b = new Point(4, 6);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when03to460thenFalse() {
         double expected = 7;
-        int x1 = 0;
-        int y1 = 3;
-        int x2 = 4;
-        int y2 = 6;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertNotEquals(expected, out, 0.0);
+        Point a = new Point(0, 3);
+        Point b = new Point(4, 6);
+        double dist = a.distance(b);
+        assertNotEquals(expected, dist, 0.0);
     }
 }
